33a55b3c97bed18a6404e42d7fb5b67569d00026
Vladimir Matushkin <matushkin.vladimir98@gmail.com>
2022-01-16 18:17:00 +0500

    Move code from <imgui_impl_dx12.h> to DX12ImGuiRenderContext

--- SuperNova/Engine/private/Engine/Renderer/DirectX12/DX12ImGuiRenderContext.hpp
+++ SuperNova/Engine/private/Engine/Renderer/DirectX12/DX12ImGuiRenderContext.hpp
@@ -3,19 +3,18 @@
 #include <Engine/Core/Core.hpp>
 #include <Engine/Renderer/IImGuiRenderContext.hpp>
 
+#include <d3d12.h>
+#include <imgui.h> // NOTE(v.matushkin): Because of ImVec2
 
-enum DXGI_FORMAT;
-struct ID3D12Device;
-struct ID3D12DescriptorHeap;
-struct ID3D12GraphicsCommandList;
-struct D3D12_CPU_DESCRIPTOR_HANDLE;
-struct D3D12_GPU_DESCRIPTOR_HANDLE;
+
+struct ImGui_ImplDX12_RenderBuffers;
+struct ImGui_ImplDX12_ViewportData;
 
 
 namespace snv
 {
 
-class DX12ImGuiRenderContext final: public IImGuiRenderContext
+class DX12ImGuiRenderContext final : public IImGuiRenderContext
 {
 public:
     DX12ImGuiRenderContext(
@@ -33,7 +32,41 @@ public:
     void EndFrame(ImDrawData* imguiDrawData) override;
 
 private:
-    ID3D12GraphicsCommandList* m_cmd; // For ImGui_ImplDX12_RenderDrawData
+    bool ImGui_ImplDX12_Init();
+    void ImGui_ImplDX12_Shutdown();
+    void ImGui_ImplDX12_NewFrame();
+    void ImGui_ImplDX12_RenderDrawData(ImDrawData* draw_data, ID3D12GraphicsCommandList* ctx);
+
+    void ImGui_ImplDX12_CreateFontsTexture();
+    bool ImGui_ImplDX12_CreateDeviceObjects();
+
+    void ImGui_ImplDX12_SetupRenderState(ImDrawData* draw_data, ID3D12GraphicsCommandList* ctx, ImGui_ImplDX12_RenderBuffers* fr);
+    static void ImGui_ImplDX12_DestroyRenderBuffers(ImGui_ImplDX12_RenderBuffers* render_buffers);
+    void ImGui_ImplDX12_InvalidateDeviceObjects();
+
+    // MULTI-VIEWPORT / PLATFORM INTERFACE SUPPORT
+    void ImGui_ImplDX12_InitPlatformInterface();
+    void ImGui_ImplDX12_ShutdownPlatformInterface();
+    static void ImGui_ImplDX12_CreateWindow(ImGuiViewport* viewport);
+    static void ImGui_ImplDX12_DestroyWindow(ImGuiViewport* viewport);
+    static void ImGui_ImplDX12_SetWindowSize(ImGuiViewport* viewport, ImVec2 size);
+    static void ImGui_ImplDX12_RenderWindow(ImGuiViewport* viewport, void*);
+    static void ImGui_ImplDX12_SwapBuffers(ImGuiViewport* viewport, void*);
+
+    // For ImGui_ImplDX12_DestroyWindow, ImGui_ImplDX12_SetWindowSize
+    static void ImGui_WaitForPendingOperations(ImGui_ImplDX12_ViewportData* vd);
+
+private:
+    ID3D12GraphicsCommandList*  m_ctx;
+    ID3D12Device*               m_pd3dDevice;
+    ID3D12RootSignature*        m_pRootSignature;
+    ID3D12PipelineState*        m_pPipelineState;
+    DXGI_FORMAT                 m_RTVFormat;
+    ID3D12Resource*             m_pFontTextureResource;
+    D3D12_CPU_DESCRIPTOR_HANDLE m_hFontSrvCpuDescHandle;
+    D3D12_GPU_DESCRIPTOR_HANDLE m_hFontSrvGpuDescHandle;
+    ID3D12DescriptorHeap*       m_pd3dSrvDescHeap;
+    ui32                        m_numFramesInFlight;
 };
 
 } // namespace snv
