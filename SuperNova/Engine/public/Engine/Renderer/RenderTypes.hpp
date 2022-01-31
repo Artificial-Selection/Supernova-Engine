Artificial-Selection/Supernova-Engine

commit af5d2dad1c4226bb7cc213a11a26a49f9c02d9c7

[SNV-21] Use DX11Backend in DX11ImGuiRenderContext to create and bind ImGui shader

SuperNova/Engine/public/Engine/Renderer/RenderTypes.hpp
@@ -140,15 +140,18 @@ enum class VertexAttribute : ui8
 
 enum class VertexAttributeFormat : ui8
 {
-    Int8,
-    Int16,
-    Int32,
+    SInt8,
+    SInt16,
+    SInt32,
     UInt8,
     UInt16,
     UInt32,
+    SNorm8,
+    SNorm16,
+    UNorm8,
+    UNorm16,
     Float16,
     Float32,
-    Float64
 };
 
 enum class RenderTextureLoadAction : ui8

SuperNova/Engine/private/Engine/Renderer/Directx11/DX11ImGuiRenderContext.hpp
@@ -2,6 +2,7 @@
 
 #include <Engine/Core/Core.hpp>
 #include <Engine/Renderer/IImGuiRenderContext.hpp>
+#include <Engine/Renderer/RenderTypes.hpp>
 
 #include <imgui.h> // NOTE(v.matushkin): Because of ImVec2
 
@@ -11,27 +12,26 @@ struct ID3D11Device5;
 struct ID3D11DeviceContext4;
 struct ID3D11Buffer;
 struct ID3D11Buffer;
-struct ID3D11VertexShader;
-struct ID3D11InputLayout;
 struct ID3D11Buffer;
-struct ID3D11PixelShader;
 struct ID3D11SamplerState;
 struct ID3D11ShaderResourceView;
-struct ID3D11RasterizerState;
-struct ID3D11BlendState;
-struct ID3D11DepthStencilState;
-
-
-// struct ImGuiViewport;
 
 
 namespace snv
 {
 
+class DX11Backend;
+
+
 class DX11ImGuiRenderContext final : public IImGuiRenderContext
 {
 public:
-    DX11ImGuiRenderContext(ID3D11Device5* d3dDevice, ID3D11DeviceContext4* d3dDeviceContext, IDXGIFactory5* d3dFactory);
+    DX11ImGuiRenderContext(
+        DX11Backend*          dx11Backend,
+        ID3D11Device5*        d3dDevice,
+        ID3D11DeviceContext4* d3dDeviceContext,
+        IDXGIFactory5*        d3dFactory
+    );
     ~DX11ImGuiRenderContext() override;
 
     void BeginFrame() override;
@@ -61,20 +61,17 @@ private:
     static void ImGui_ImplDX11_SwapBuffers(ImGuiViewport* viewport, void*);
 
 private:
+    DX11Backend*              m_dx11Backend;
+    ShaderHandle              m_imguiShaderHandle;
+
     ID3D11Device5*            m_pd3dDevice;
     ID3D11DeviceContext4*     m_pd3dDeviceContext;
     IDXGIFactory5*            m_pFactory;
     ID3D11Buffer*             m_pVB;
     ID3D11Buffer*             m_pIB;
-    ID3D11VertexShader*       m_pVertexShader;
-    ID3D11InputLayout*        m_pInputLayout;
     ID3D11Buffer*             m_pVertexConstantBuffer;
-    ID3D11PixelShader*        m_pPixelShader;
     ID3D11SamplerState*       m_pFontSampler;
     ID3D11ShaderResourceView* m_pFontTextureView;
-    ID3D11RasterizerState*    m_pRasterizerState;
-    ID3D11BlendState*         m_pBlendState;
-    ID3D11DepthStencilState*  m_pDepthStencilState;
     i32                       m_VertexBufferSize;
     i32                       m_IndexBufferSize;
 };

SuperNova/Engine/private/Engine/Renderer/Directx11/DX11ShaderCompiler.hpp
@@ -2,6 +2,7 @@
 
 #include <Engine/Core/Core.hpp>
 
+#include <string>
 #include <vector>
 
 
@@ -23,8 +24,8 @@ class DX11ShaderCompiler
 public:
     struct ShaderBuffer
     {
-        void* Ptr;
-        ui64  Size;
+        const void* Ptr;
+        ui64        Size;
     };
 
 
@@ -32,16 +33,22 @@ public:
     ~DX11ShaderCompiler();
 
     [[nodiscard]] ShaderBuffer GetVertexShaderBuffer() const;
-    [[nodiscard]] ShaderBuffer GetPixelShaderBuffer()  const;
+    [[nodiscard]] ShaderBuffer GetPixelShaderBuffer() const;
 
     [[nodiscard]] std::vector<D3D11_INPUT_ELEMENT_DESC> GetInputLayoutDesc() const;
 
 private:
-    ID3DBlob* m_vertexShaderBlob;
-    ID3DBlob* m_pixelShaderBlob;
+    [[nodiscard]] static  ID3DBlob* CompileShader(
+        const std::string& shaderStageSource,
+        const char* shaderName,
+        const char* shaderTarget
+    );
 
-    bool                    m_separateStreams;
+private:
+    ID3DBlob*               m_vertexShaderBlob;
+    ID3DBlob*               m_pixelShaderBlob;
     ID3D11ShaderReflection* m_vertexShaderReflection;
+    const bool              m_isImGuiShader;
 };
 
 } // namespace snv

SuperNova/Engine/private/Engine/Renderer/RenderDefaults.hpp
@@ -2,12 +2,64 @@
 
 #include <Engine/Renderer/RenderTypes.hpp>
 
+#include <string_view>
 
-namespace snv
-{
 
-namespace RenderDefaults
+namespace snv::RenderDefaults
 {
+    inline const std::string_view ImGuiShaderName = "Engine/ImGui";
+
+
+    struct EngineInputLayout
+    {
+        VertexAttributeFormat PositionFormat;
+        ui8                   PositionDimension;
+        ui8                   PositionStream; // NOTE(v.matushkin): Stream/InputSlot/Location/Binding which is the good name?
+        VertexAttributeFormat NormalFormat;
+        ui8                   NormalDimension;
+        ui8                   NormalStream;
+        VertexAttributeFormat TexCoor0Format;
+        ui8                   TexCoor0Dimension;
+        ui8                   TexCoor0Stream;
+    };
+    struct ImGuiInputLayout
+    {
+        VertexAttributeFormat PositionFormat;
+        ui8                   PositionDimension;
+        ui8                   PositionOffset;
+        VertexAttributeFormat TexCoor0Format;
+        ui8                   TexCoor0Dimension;
+        ui8                   TexCoor0Offset;
+        VertexAttributeFormat ColorFormat;
+        ui8                   ColorDimension;
+        ui8                   ColorOffset;
+    };
+
+    inline const EngineInputLayout EngineVertexInputLayout = {
+        .PositionFormat    = VertexAttributeFormat::Float32,
+        .PositionDimension = 3,
+        .PositionStream    = 0,
+        .NormalFormat      = VertexAttributeFormat::Float32,
+        .NormalDimension   = 3,
+        .NormalStream      = 1,
+        .TexCoor0Format    = VertexAttributeFormat::Float32,
+        .TexCoor0Dimension = 3,
+        .TexCoor0Stream    = 2,
+    };
+
+    inline const ImGuiInputLayout ImGuiVertexInputLayout = {
+        .PositionFormat    = VertexAttributeFormat::Float32,
+        .PositionDimension = 2,
+        .PositionOffset    = 0,
+        .TexCoor0Format    = VertexAttributeFormat::Float32,
+        .TexCoor0Dimension = 2,
+        .TexCoor0Offset    = 8,
+        .ColorFormat       = VertexAttributeFormat::UNorm8,
+        .ColorDimension    = 4,
+        .ColorOffset       = 16,
+    };
+
+
     // NOTE(v.matushkin): Shoud be in BlendStateDesc
     inline const bool IndependentBlendEnable = false; // NOTE(v.matushkin): Need a lot of checks to support it
 
@@ -35,6 +87,5 @@ namespace RenderDefaults
         .AlphaBlendOp        = BlendOp::Add,
         .LogicOp             = BlendLogicOp::Noop,
     };
-}
 
-} // namespace snv
+} // namespace snv::RenderDefaults

SuperNova/Engine/source/Renderer/DirectX12/DX12Backend.cpp
@@ -1123,19 +1123,14 @@ ShaderHandle DX12Backend::CreateShader(const ShaderDesc& shaderDesc)
     //- BlendState
     const auto blendStateDesc = shaderDesc.BlendStateDesc;
 
-    D3D12_RENDER_TARGET_BLEND_DESC d3dRenderTargetBlendDesc;
-    // NOTE(v.matushkin): May be there is no need to set this if BlendMode::Off ?
-    d3dRenderTargetBlendDesc.RenderTargetWriteMask = D3D12_COLOR_WRITE_ENABLE_ALL;
+    D3D12_RENDER_TARGET_BLEND_DESC d3dRenderTargetBlendDesc = {
+        // NOTE(v.matushkin): May be there is no need to set this if BlendMode::Off ?
+        .RenderTargetWriteMask = D3D12_COLOR_WRITE_ENABLE_ALL,
+    };
 
-    if (blendStateDesc.BlendMode == BlendMode::Off)
-    {
-        d3dRenderTargetBlendDesc.BlendEnable   = false;
-        d3dRenderTargetBlendDesc.LogicOpEnable = false;
-    }
-    else if (blendStateDesc.BlendMode == BlendMode::BlendOp)
+    if (blendStateDesc.BlendMode == BlendMode::BlendOp)
     {
         d3dRenderTargetBlendDesc.BlendEnable    = true;
-        d3dRenderTargetBlendDesc.LogicOpEnable  = false;
         d3dRenderTargetBlendDesc.SrcBlend       = dx12_BlendFactor(blendStateDesc.ColorSrcBlendFactor);
         d3dRenderTargetBlendDesc.DestBlend      = dx12_BlendFactor(blendStateDesc.ColorDstBlendFactor);
         d3dRenderTargetBlendDesc.BlendOp        = dx12_BlendOp(blendStateDesc.ColorBlendOp);
@@ -1143,9 +1138,8 @@ ShaderHandle DX12Backend::CreateShader(const ShaderDesc& shaderDesc)
         d3dRenderTargetBlendDesc.DestBlendAlpha = dx12_BlendFactor(blendStateDesc.AlphaDstBlendFactor);
         d3dRenderTargetBlendDesc.BlendOpAlpha   = dx12_BlendOp(blendStateDesc.AlphaBlendOp);
     }
-    else // blendStateDesc.BlendMode == BlendMode::LogicOp
+    else if (blendStateDesc.BlendMode == BlendMode::LogicOp)
     {
-        d3dRenderTargetBlendDesc.BlendEnable   = false;
         d3dRenderTargetBlendDesc.LogicOpEnable = true;
         d3dRenderTargetBlendDesc.LogicOp       = dx12_BlendLogicOp(blendStateDesc.LogicOp);
     }
@@ -1154,6 +1148,7 @@ ShaderHandle DX12Backend::CreateShader(const ShaderDesc& shaderDesc)
         .AlphaToCoverageEnable  = false,
         .IndependentBlendEnable = RenderDefaults::IndependentBlendEnable,
     };
+    // TODO(v.matushkin): Get RenderTargets count from reflection and set only needed, not all?
     for (ui32 i = 0; i < D3D12_SIMULTANEOUS_RENDER_TARGET_COUNT; ++i)
     {
         d3dBlendDesc.RenderTarget[i] = d3dRenderTargetBlendDesc;

SuperNova/Engine/source/Renderer/Directx11/DX11Backend.cpp
@@ -222,6 +222,11 @@ DX11Backend::DX11Backend()
 
     //- Set default state
     m_deviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+    m_deviceContext->GSSetShader(nullptr, nullptr, 0);
+    m_deviceContext->HSSetShader(nullptr, nullptr, 0);
+    m_deviceContext->DSSetShader(nullptr, nullptr, 0);
+    m_deviceContext->CSSetShader(nullptr, nullptr, 0);
 }
 
 // NOTE(v.matushkin): Doubt I need this
@@ -381,7 +386,7 @@ void DX11Backend::DrawBuffer(TextureHandle textureHandle, BufferHandle bufferHan
 
 IImGuiRenderContext* DX11Backend::CreateImGuiRenderContext()
 {
-    return new DX11ImGuiRenderContext(m_device.Get(), m_deviceContext.Get(), m_factory.Get());
+    return new DX11ImGuiRenderContext(this, m_device.Get(), m_deviceContext.Get(), m_factory.Get());
 }
 
 
@@ -631,18 +636,17 @@ ShaderHandle DX11Backend::CreateShader(const ShaderDesc& shaderDesc)
     ID3D11RasterizerState2* d3dRasterizerState;
     {
         const auto rasterizerStateDesc = shaderDesc.RasterizerStateDesc;
+        const auto isImGuiShader       = shaderDesc.Name == RenderDefaults::ImGuiShaderName;
 
-        // NOTE(v.matuskin): Changed FrontCounterClockwise to true, everything else is default values
-        //  FrontCounterClockwise is false by default, and in OpenGL it's true by default
         D3D11_RASTERIZER_DESC2 d3dRasterizerStateDesc = {
             .FillMode              = dx11_PolygonMode(rasterizerStateDesc.PolygonMode),
             .CullMode              = dx11_CullMode(rasterizerStateDesc.CullMode),
-            .FrontCounterClockwise = dx11_TriangleFrontFace(rasterizerStateDesc.FrontFace),
+            .FrontCounterClockwise = !isImGuiShader,
             .DepthBias             = D3D11_DEFAULT_DEPTH_BIAS,
             .DepthBiasClamp        = D3D11_DEFAULT_DEPTH_BIAS_CLAMP,
             .SlopeScaledDepthBias  = D3D11_DEFAULT_SLOPE_SCALED_DEPTH_BIAS,
             .DepthClipEnable       = true,
-            .ScissorEnable         = false, // NOTE(v.matushkin): ImGui needs 'true' ?
+            .ScissorEnable         = isImGuiShader,
             .MultisampleEnable     = false,
             .AntialiasedLineEnable = false,
             .ForcedSampleCount     = 0,
@@ -688,34 +692,25 @@ ShaderHandle DX11Backend::CreateShader(const ShaderDesc& shaderDesc)
     {
         const auto blendStateDesc = shaderDesc.BlendStateDesc;
 
-        D3D11_RENDER_TARGET_BLEND_DESC1 d3dRenderTargetBlendDesc;
-        // NOTE(v.matushkin): May be there is no need to set this if BlendMode::Off ?
-        d3dRenderTargetBlendDesc.RenderTargetWriteMask = D3D11_COLOR_WRITE_ENABLE_ALL;
-        if (blendStateDesc.BlendMode == BlendMode::Off)
-        {
-            d3dRenderTargetBlendDesc.BlendEnable   = false;
-            d3dRenderTargetBlendDesc.LogicOpEnable = false;
-        }
-        else
-        {
-            if (blendStateDesc.BlendMode == BlendMode::BlendOp)
-            {
-                d3dRenderTargetBlendDesc.BlendEnable   = true;
-                d3dRenderTargetBlendDesc.LogicOpEnable = false;
-                d3dRenderTargetBlendDesc.BlendOp       = dx11_BlendOp(blendStateDesc.ColorBlendOp);
-                d3dRenderTargetBlendDesc.BlendOpAlpha  = dx11_BlendOp(blendStateDesc.AlphaBlendOp);
-            }
-            else // BlendMode::LogicOp
-            {
-                d3dRenderTargetBlendDesc.BlendEnable   = false;
-                d3dRenderTargetBlendDesc.LogicOpEnable = true;
-                d3dRenderTargetBlendDesc.LogicOp       = dx11_BlendLogicOp(blendStateDesc.LogicOp);
-            }
+        D3D11_RENDER_TARGET_BLEND_DESC1 d3dRenderTargetBlendDesc = {
+            // NOTE(v.matushkin): May be there is no need to set this if BlendMode::Off ?
+            .RenderTargetWriteMask = D3D11_COLOR_WRITE_ENABLE_ALL,
+        };
 
+        if (blendStateDesc.BlendMode == BlendMode::BlendOp)
+        {
+            d3dRenderTargetBlendDesc.BlendEnable    = true;
             d3dRenderTargetBlendDesc.SrcBlend       = dx11_BlendFactor(blendStateDesc.ColorSrcBlendFactor);
             d3dRenderTargetBlendDesc.DestBlend      = dx11_BlendFactor(blendStateDesc.ColorDstBlendFactor);
+            d3dRenderTargetBlendDesc.BlendOp        = dx11_BlendOp(blendStateDesc.ColorBlendOp);
             d3dRenderTargetBlendDesc.SrcBlendAlpha  = dx11_BlendFactor(blendStateDesc.AlphaSrcBlendFactor);
             d3dRenderTargetBlendDesc.DestBlendAlpha = dx11_BlendFactor(blendStateDesc.AlphaDstBlendFactor);
+            d3dRenderTargetBlendDesc.BlendOpAlpha   = dx11_BlendOp(blendStateDesc.AlphaBlendOp);
+        }
+        else if (blendStateDesc.BlendMode == BlendMode::LogicOp)
+        {
+            d3dRenderTargetBlendDesc.LogicOpEnable = true;
+            d3dRenderTargetBlendDesc.LogicOp       = dx11_BlendLogicOp(blendStateDesc.LogicOp);
         }
 
         D3D11_BLEND_DESC1 d3dBlendStateDesc;

commit 829bf9fbbeb58e4bbcb84584b7dd074ee6a24607

[SNV-21] Fix a bug with GLBuffer creation introduced in af5d2da

SuperNova/Engine/public/Engine/Renderer/RenderTypes.hpp
@@ -138,6 +138,7 @@ enum class VertexAttribute : ui8
     TexCoord0,
 };
 
+// NOTE(v.matushkin): Seems like OpenGL/Vulkan support more formats (VK_FORMAT_R64_SFLOAT for example)
 enum class VertexAttributeFormat : ui8
 {
     SInt8,

SuperNova/Engine/source/Renderer/OpenGL/GLBuffer.cpp
@@ -11,17 +11,31 @@
 #define UINT_TO_VOID_PTR(ui) (void*)(ui64)(ui)
 
 
-constexpr ui32 gl_VertexAttributeFormat[] = {
-    GL_BYTE ,          // VertexAttributeFormat::Int8
-    GL_SHORT,          // VertexAttributeFormat::Int16
-    GL_INT,            // VertexAttributeFormat::Int32
-    GL_UNSIGNED_BYTE,  // VertexAttributeFormat::UInt8
-    GL_UNSIGNED_SHORT, // VertexAttributeFormat::UInt16
-    GL_UNSIGNED_INT,   // VertexAttributeFormat::UInt32
-    GL_HALF_FLOAT,     // VertexAttributeFormat::Float16
-    GL_FLOAT,          // VertexAttributeFormat::Float32
-    GL_DOUBLE          // VertexAttributeFormat::Float64
+struct GLVertexAttribute
+{
+    ui32 Format;
+    bool Normalized;
 };
+
+static GLVertexAttribute gl_VertexAttribute(snv::VertexAttributeFormat vertexAttributeFormat)
+{
+    static const GLVertexAttribute gl_VertexAttribute[] = {
+        {GL_BYTE,           false},
+        {GL_SHORT,          false},
+        {GL_INT,            false},
+        {GL_UNSIGNED_BYTE,  false},
+        {GL_UNSIGNED_SHORT, false},
+        {GL_UNSIGNED_INT,   false},
+        {GL_BYTE,           true},
+        {GL_SHORT,          true},
+        {GL_UNSIGNED_BYTE,  true},
+        {GL_UNSIGNED_SHORT, true},
+        {GL_HALF_FLOAT,     false},
+        {GL_FLOAT,          false},
+    };
+
+    return gl_VertexAttribute[static_cast<ui8>(vertexAttributeFormat)];
+}
 
 
 namespace snv
@@ -54,10 +68,10 @@ GLBuffer::GLBuffer(
     for (const auto& vertexAttribute : vertexLayout)
     {
         const auto attribute = static_cast<ui8>(vertexAttribute.Attribute);
-        const auto format    = gl_VertexAttributeFormat[static_cast<ui8>(vertexAttribute.Format)];
-        
+        const auto [format, normalized] = gl_VertexAttribute(vertexAttribute.Format);
+
         glEnableVertexAttribArray(attribute);
-        glVertexAttribPointer(attribute, vertexAttribute.Dimension, format, GL_FALSE, 0, UINT_TO_VOID_PTR(vertexAttribute.Offset));
+        glVertexAttribPointer(attribute, vertexAttribute.Dimension, format, normalized, 0, UINT_TO_VOID_PTR(vertexAttribute.Offset));
     }
 }
 